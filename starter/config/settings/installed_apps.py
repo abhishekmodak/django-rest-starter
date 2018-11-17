f93eab70b049b7e30e0c258b1f2f997c8f0f69b0
Abhishek Modak <amodak828@gmail.com>
2018-11-17 16:43:19 +0530

    keeping installed apps in a different file

---
+++ starter/config/settings/installed_apps.py
@@ -0,0 +1,18 @@
+# Apps introduced by Django
+DJANGO_APPS = [
+                'django.contrib.admin',
+                'django.contrib.auth',
+                'django.contrib.contenttypes',
+                'django.contrib.sessions',
+                'django.contrib.messages',
+                'django.contrib.staticfiles',
+            ]
+
+# THIRD PARTY APPS
+THIRD_PARTY_APPS = []
+
+# Apps created by you in this project
+CREATED_APPS = []
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + CREATED_APPS
+
